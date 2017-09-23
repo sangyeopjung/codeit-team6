eaa4ca87b318bcc1340611a82f5e847bb13d19bf
Sang yeop Jung <syjung812@gmail.com>
2017-09-24 03:42:39 +0800

    q4

--- routes/question4.js
+++ routes/question4.js
@@ -27,7 +27,7 @@ router.post('/stringcompression/:mode', function(req, res) {
         encoded = encoded.toString();
         encoded = encoded.replace(/,/g, "");
 
-        var len = (encoded.length-1) * 8;
+        var len = encoded.length * 8;
         res.format({
             'text/plain': function() {
                 //res.send(encoded);
@@ -39,7 +39,6 @@ router.post('/stringcompression/:mode', function(req, res) {
         encoded = lzwcompress.pack(data);
         console.log(encoded);
         var len = (encoded.length-1) * 12;
-        len += 8;
         res.format({
             'text/plain': function() {
                 res.send(len.toString());
