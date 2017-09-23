cd73f0736b1e6d1fe5fe1e0a573b2b96aee5382f
Sang yeop Jung <syjung812@gmail.com>
2017-09-23 14:30:41 +0800

    q1 timsort

--- routes/question1.js
+++ routes/question1.js
@@ -1,12 +1,13 @@
 var express = require('express');
-var quickSort = require('js-quicksort')
+//var quickSort = require('js-quicksort')
+var timsort = require('timsort')
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/sort', function(req, res, next) {
   console.log(req.body);
   var input = req.body;
-  res.send(quickSort(input));
+  res.send(timsort.sort(input));
 });
 
 module.exports = router;
