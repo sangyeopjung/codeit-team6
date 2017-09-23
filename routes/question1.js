42b722c729315debee7cc133d7132b06469fc8ec
Sang yeop Jung <syjung812@gmail.com>
2017-09-24 02:47:03 +0800

    q1

--- routes/question1.js
+++ routes/question1.js
@@ -132,56 +132,49 @@ var router = express.Router();
 //     return array;
 // }
 
-// var extractDigit = function( a, bitMask, shiftRightAmount ) {
-//     var digit = (a & bitMask) >>> shiftRightAmount; // extract the digit we are sorting based on
-//     return digit;
-// }
-//
-// function radixSortLSD(_input_array) {
-//     var numberOfBins = 256;
-//     var Log2ofPowerOfTwoRadix = 8;
-//     var _output_array = new Array(_input_array.length);
-//     var count = new Array(numberOfBins);
-//     var _output_array_has_result = false;
-//
-//     var bitMask = 255;
-//     var shiftRightAmount = 0;
-//
-//     var startOfBin = new Array( numberOfBins );
-//     var endOfBin   = new Array( numberOfBins );
-//
-//     while( bitMask != 0 ) // end processing digits when all the mask bits have been processed and shifted out, leaving no bits set in the bitMask
-//     {
-//         for (var i = 0; i < numberOfBins; i++ )
-//             count[ i ] = 0;
-//         for (var _current = 0; _current < _input_array.length; _current++ ) // Scan the array and count the number of times each digit value appears - i.e. size of each bin
-//             count[ extractDigit( _input_array[ _current ], bitMask, shiftRightAmount ) ]++;
-//
-//         startOfBin[ 0 ] = endOfBin[ 0 ] = 0;
-//         for( var i = 1; i < numberOfBins; i++ )
-//             startOfBin[ i ] = endOfBin[ i ] = startOfBin[ i - 1 ] + count[ i - 1 ];
-//         for ( var _current = 0; _current < _input_array.length; _current++ )
-//             _output_array[ endOfBin[ extractDigit( _input_array[ _current ], bitMask, shiftRightAmount ) ]++ ] = _input_array[ _current ];
-//
-//         bitMask <<= Log2ofPowerOfTwoRadix;
-//         shiftRightAmount += Log2ofPowerOfTwoRadix;
-//         _output_array_has_result = !_output_array_has_result;
-//
-//         var tmp = _input_array, _input_array = _output_array, _output_array = tmp; // swap input and output arrays
-//     }
-//     if ( _output_array_has_result )
-//         for ( var _current = 0; _current < _input_array.length; _current++ ) // copy from output array into the input array
-//             _input_array[ _current ] = _output_array[ _current ];
-//
-//     var n = _input_array.length;
-//     for(var i = 0; i < n / 2; i++) {
-//         var tmp = _input_array[i];
-//         _input_array[i] = _input_array[n - i - 1];
-//         _input_array[n - i - 1 ] = tmp;
-//     }
-//
-//     return _input_array;
-// }
+var extractDigit = function( a, bitMask, shiftRightAmount ) {
+    var digit = (a & bitMask) >>> shiftRightAmount; // extract the digit we are sorting based on
+    return digit;
+}
+
+function radixSortLSD(_input_array) {
+    var numberOfBins = 256;
+    var Log2ofPowerOfTwoRadix = 8;
+    var _output_array = new Array(_input_array.length);
+    var count = new Array(numberOfBins);
+    var _output_array_has_result = false;
+
+    var bitMask = 255;
+    var shiftRightAmount = 0;
+
+    var startOfBin = new Array( numberOfBins );
+    var endOfBin   = new Array( numberOfBins );
+
+    while( bitMask != 0 ) // end processing digits when all the mask bits have been processed and shifted out, leaving no bits set in the bitMask
+    {
+        for (var i = 0; i < numberOfBins; i++ )
+            count[ i ] = 0;
+        for (var _current = 0; _current < _input_array.length; _current++ ) // Scan the array and count the number of times each digit value appears - i.e. size of each bin
+            count[ extractDigit( _input_array[ _current ], bitMask, shiftRightAmount ) ]++;
+
+        startOfBin[ 0 ] = endOfBin[ 0 ] = 0;
+        for( var i = 1; i < numberOfBins; i++ )
+            startOfBin[ i ] = endOfBin[ i ] = startOfBin[ i - 1 ] + count[ i - 1 ];
+        for ( var _current = 0; _current < _input_array.length; _current++ )
+            _output_array[ endOfBin[ extractDigit( _input_array[ _current ], bitMask, shiftRightAmount ) ]++ ] = _input_array[ _current ];
+
+        bitMask <<= Log2ofPowerOfTwoRadix;
+        shiftRightAmount += Log2ofPowerOfTwoRadix;
+        _output_array_has_result = !_output_array_has_result;
+
+        var tmp = _input_array, _input_array = _output_array, _output_array = tmp; // swap input and output arrays
+    }
+    if ( _output_array_has_result )
+        for ( var _current = 0; _current < _input_array.length; _current++ ) // copy from output array into the input array
+            _input_array[ _current ] = _output_array[ _current ];
+
+    return _input_array;
+}
 
 function countingSort(arr, min, max) {
     var i, z = 0, count = [];
@@ -206,7 +199,8 @@ router.post('/sort', function(req, res, next) {
     for (var i = 0; i < sort.length; i++)
         sort[i] += 10000;
 
-    sort = countingSort(sort, 0, 20010);
+    //sort = countingSort(sort, 0, 20010);
+    sort = radixSortLSD(sort);
 
     for (var i = 0; i < sort.length; i++)
         sort[i] -= 10000;
