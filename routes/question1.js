fe51b4e9e0505757245cbf8d9d907ccb1654e5d7
Sang yeop Jung <syjung812@gmail.com>
2017-09-23 13:59:42 +0800

    q1 fix

--- routes/question1.js
+++ routes/question1.js
@@ -1,42 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-function quickSort(arr, left, right)
-{
-    var i = left;
-    var j = right;
-    var tmp;
-    var pivotidx = (left + right) / 2;
-    var pivot = parseInt(arr[pivotidx.toFixed()]);
-    /* partition */
-    while (i <= j)
-    {
-        while (parseInt(arr[i]) < pivot)
-            i++;
-        while (parseInt(arr[j]) > pivot)
-            j--;
-        if (i <= j)
-        {
-            tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            i++;
-            j--;
-        }
-    }
-
-    /* recursion */
-    if (left < j)
-        quickSort(arr, left, j);
-    if (i < right)
-        quickSort(arr, i, right);
-    return arr;
-}
-
 router.post('/sort', function(req, res, next) {
   console.log(req.body);
   var out = req.body;
-  res.send(quickSort(out, 0, out.length-1));
+  out = out.sort(function(a, b) { return a - b; });
+  res.send(out);
 });
 
 module.exports = router;
