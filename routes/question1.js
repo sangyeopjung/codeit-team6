766ab80f78b31cbb2fe4e43541d531fc9fce4a43
Chris Park <Chris@Chriss-MacBook-Pro.local>
2017-09-23 10:43:11 +0800

    q1

--- routes/question1.js
+++ routes/question1.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var quickSort = require('js-quicksort')
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/sort', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(req.body);
+  var input = [];
+  for (var i = 0; i < req.body.length; i++){
+    input[i] = req.body[i];
+  }
+  res.send(quickSort(input));
 });
 
 module.exports = router;
