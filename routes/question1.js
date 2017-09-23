a9192a99260cdab5a3c6eb98b5d81e99e48a2bc8
Sang yeop Jung <syjung812@gmail.com>
2017-09-23 13:38:55 +0800

    q1

--- routes/question1.js
+++ routes/question1.js
@@ -5,13 +5,10 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/sort', function(req, res, next) {
   console.log(req.body);
-  var input = [];
-  for (var i = 0; i < req.body.length; i++){
-    input.push(req.body[i]);
-  }
-  var sort = quickSort(input);
-  console.log(sort);
-  res.send(sort);
+  var input = req.body;
+  input.sort();
+  console.log(input);
+  res.send(input);
 });
 
 module.exports = router;
