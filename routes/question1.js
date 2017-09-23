a74c153299e349437e2af84b14d6a48bbf14041b
Sang yeop Jung <syjung812@gmail.com>
2017-09-23 20:34:57 +0800

    q1 radix

--- routes/question1.js
+++ routes/question1.js
@@ -1,110 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-// function quickSort(arr, left, right){
-//     var len = arr.length,
-//         pivot,
-//         partitionIndex;
-//
-//
-//     if(left < right){
-//         pivot = right;
-//         partitionIndex = partition(arr, pivot, left, right);
-//
-//         //sort left and right
-//         quickSort(arr, left, partitionIndex - 1);
-//         quickSort(arr, partitionIndex + 1, right);
-//     }
-//     return arr;
-// }
-//
-// function partition(arr, pivot, left, right){
-//     var pivotValue = arr[pivot],
-//         partitionIndex = left;
-//
-//     for(var i = left; i < right; i++){
-//         if(arr[i] < pivotValue){
-//             swap(arr, i, partitionIndex);
-//             partitionIndex++;
-//         }
-//     }
-//     swap(arr, right, partitionIndex);
-//     return partitionIndex;
-// }
-//
-// function swap(arr, i, j){
-//     var temp = arr[i];
-//     arr[i] = arr[j];
-//     arr[j] = temp;
-// }
+function quickSort(arr, left, right){
+    var len = arr.length,
+        pivot,
+        partitionIndex;
 
-function radixBucketSort (arr) {
-    var idx1, idx2, idx3, len1, len2, radix, radixKey;
-    var radices = {}, buckets = {}, num, curr;
-    var currLen, radixStr, currBucket;
 
-    len1 = arr.length;
-    len2 = 10;  // radix sort uses ten buckets
+    if(left < right){
+        pivot = right;
+        partitionIndex = partition(arr, pivot, left, right);
 
-    // find the relevant radices to process for efficiency
-    for (idx1 = 0;idx1 < len1;idx1++) {
-        radices[arr[idx1].toString().length] = 0;
+        //sort left and right
+        quickSort(arr, left, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, right);
     }
+    return arr;
+}
 
-    // loop for each radix. For each radix we put all the items
-    // in buckets, and then pull them out of the buckets.
-    for (radix in radices) {
-        // put each array item in a bucket based on its radix value
-        len1 = arr.length;
-        for (idx1 = 0;idx1 < len1;idx1++) {
-            curr = arr[idx1];
-            // item length is used to find its current radix value
-            currLen = curr.toString().length;
-            // only put the item in a radix bucket if the item
-            // key is as long as the radix
-            if (currLen >= radix) {
-                // radix starts from beginning of key, so need to
-                // adjust to get redix values from start of stringified key
-                radixKey = curr.toString()[currLen - radix];
-                // create the bucket if it does not already exist
-                if (!buckets.hasOwnProperty(radixKey)) {
-                    buckets[radixKey] = [];
-                }
-                // put the array value in the bucket
-                buckets[radixKey].push(curr);
-            } else {
-                if (!buckets.hasOwnProperty('0')) {
-                    buckets['0'] = [];
-                }
-                buckets['0'].push(curr);
-            }
+function partition(arr, pivot, left, right){
+    var pivotValue = arr[pivot],
+        partitionIndex = left;
+
+    for(var i = left; i < right; i++){
+        if(arr[i] < pivotValue){
+            swap(arr, i, partitionIndex);
+            partitionIndex++;
         }
-        // for current radix, items are in buckets, now put them
-        // back in the array based on their buckets
-        // this index moves us through the array as we insert items
-        idx1 = 0;
-        // go through all the buckets
-        for (idx2 = 0;idx2 < len2;idx2++) {
-            // only process buckets with items
-            if (buckets[idx2] != null) {
-                currBucket = buckets[idx2];
-                // insert all bucket items into array
-                len1 = currBucket.length;
-                for (idx3 = 0;idx3 < len1;idx3++) {
-                    arr[idx1++] = currBucket[idx3];
-                }
-            }
-        }
-        buckets = {};
     }
+    swap(arr, right, partitionIndex);
+    return partitionIndex;
+}
+
+function swap(arr, i, j){
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
 router.post('/sort', function(req, res, next) {
   console.log(req.body);
   res.connection.setTimeout(10000);
   var sort = req.body;
-  //res.send(quickSort(sort, 0, sort.length-1));
-  res.send(radixBucketSort(sort).reverse());
+  res.send(quickSort(sort, 0, sort.length-1));
 });
 
 module.exports = router;
