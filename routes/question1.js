2c70e4a5ab995a636dd3be469f52b386321a3249
Sang yeop Jung <syjung812@gmail.com>
2017-09-23 14:21:18 +0800

    q1 handle large data

--- routes/question1.js
+++ routes/question1.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var quickSort = require('js-quicksort')
 var router = express.Router();
 
+/* GET users listing. */
 router.post('/sort', function(req, res, next) {
   console.log(req.body);
-  var out = req.body;
-  out = out.sort(function(a, b) { return a - b; });
-  res.send(out);
+  var input = req.body;
+  res.send(quickSort(input));
 });
 
 module.exports = router;
