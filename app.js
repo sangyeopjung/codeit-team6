c99e20f57d6f9769dd82339cad5800e1c25f875f
Sang yeop Jung <syjung812@gmail.com>
2017-09-23 14:13:57 +0800

    q1 handle large data

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.raw({limit:'5mb'}));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
