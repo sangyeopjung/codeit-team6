ca396e80b4d82ee351239e347f93c695cd5faeb2
Jin Sung KIM <jinsung515@gmail.com>
2017-09-24 11:02:30 +0800

    q4 bug fix

--- routes/question4.js
+++ routes/question4.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var lzwcompress = require('lzwcompress');
 var _ = require('underscore');
+var asciiStringSplit = require('ascii-string-split');
 var router = express.Router();
 
 router.post('/stringcompression/:mode', function(req, res) {
@@ -46,16 +47,18 @@ router.post('/stringcompression/:mode', function(req, res) {
         });
     } else if (mode == 'WDE') {
         console.log('wde', req.body.data)
-        var strArr = data.split(/(\s+)/);
+        var strArr = data.split(/([^A-Za-z])/);
         var dict = {};
         var numNonword = 0;
         var dictSize = 0;
         var numWord = 0;
 
-        console.log(req.body.data);
-        
+        // var charac = (asciiStringSplit(req.body.data, 2));
+        // console.log(charac);
+        console.log(strArr);
+
         for (var i = 0; i < strArr.length; i++){
-            if (/^[^\W\d\s]/.test(strArr[i])) {
+            if (/^[^A-Za-z]/.test(strArr[i]) ==  false) {
                 var currentWord = strArr[i];
                 if (currentWord in dict) {
                     dict["" + strArr[i]] += 1;
@@ -78,6 +81,7 @@ router.post('/stringcompression/:mode', function(req, res) {
                 res.send(len.toString());
             }
         })
+
     } else {
         res.sendStatus(400).send({"message":"Bad request"});
     }
